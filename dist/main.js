f9c937c3656ed07faa5007aba1d442383bcc5c8f
fahrianmurtazam <fahrian22@mhs.usk.ac.id>
2024-06-25 23:03:17 +0700

    update fitur terbaru

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,57 @@
-const hamburger = document.querySelector("#hamburger-menu");
-const navMenu = document.querySelector("#nav-menu");
+// tombol search
+const searchBtn = document.querySelector("#search-btn");
+const searchForm = document.querySelector("#search-form");
+const searchBox = document.querySelector("#search-box");
 
-hamburger.addEventListener("click", function () {
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("scale-x-0");
+  searchBox.focus();
+});
+// toggle shopping cart
+const cartBtn = document.querySelector('#shopping-cart-btn')
+const shoppingCart = document.querySelector('.shopping-cart')
+cartBtn.addEventListener('click', function(){
+  shoppingCart.classList.toggle('translate-x-[1000px]')
+})
+// klik diluar
+const hm = document.querySelector("#hamburger-menu");
+const navMenu = document.querySelector("#nav-menu");
+const sb = document.querySelector("#search-btn");
+const sc = document.querySelector("#shopping-cart-btn");
+hm.addEventListener("click", function () {
   navMenu.classList.toggle("translate-x-[-1000px]");
 });
 
 // klik diluar untuk hilangkan nav-menu
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+  if (!hm.contains(e.target) && !navMenu.contains(e.target)) {
     navMenu.classList.add("translate-x-[-1000px]");
   }
-});
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.add("scale-x-0");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.add("translate-x-[1000px]");
+}});
+
+// modal box
+const itemDetailBtns = document.querySelectorAll("#item-detail-btn")
+const itemModal = document.querySelector("#item-detail-modal")
+
+itemDetailBtns.forEach((btn) =>{
+  btn.addEventListener("click", function(){
+    itemModal.style.display="flex"
+  })
+})
+
+// tombol close modal
+const closeBtn = document.querySelector('.close-icon')
+closeBtn.addEventListener('click', function(){
+  itemModal.style.display="none"
+})
+
+window.addEventListener('click', function(e){
+  if(e.target === itemModal){
+    itemModal.style.display="none"
+  }
+})
